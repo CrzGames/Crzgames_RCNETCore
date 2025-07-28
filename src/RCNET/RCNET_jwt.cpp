d5d13ef7d20c094f655e450dfd1e2d2da92bccc3
Corentin Recanzone <corentindevpro@hotmail.com>
2025-07-28 21:14:04 +0200

    m

--- src/RCNET/RCNET_jwt.cpp
+++ src/RCNET/RCNET_jwt.cpp
@@ -1,16 +1,11 @@
 #include <jwt-cpp/jwt.h>
-#include <nlohmann/json.hpp>
-#include <jwt-cpp/traits/nlohmann-json/traits.h>
 #include <iostream>
-
-using namespace jwt::traits;
 
 void test(void)
 {
     std::string const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-1L-FsOjtR6uE-L4E9zJutMWKIe1v1M";
-    auto decoded = jwt::decode<nlohmann_json_traits>(token);
+    auto decoded = jwt::decode(token);
 
-    for (auto& e : decoded.get_payload_json()) {
+    for(auto& e : decoded.get_payload_json())
         std::cout << e.first << " = " << e.second << '\n';
-    }
 }
