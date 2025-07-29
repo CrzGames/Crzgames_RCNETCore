5d7179bce9bcc5d3c95e022cbcdd7bed9eb03f3c
Corentin Recanzone <corentindevpro@hotmail.com>
2025-07-29 15:36:07 +0200

    m

--- src/RCNET/RCNET_engine.cpp
+++ src/RCNET/RCNET_engine.cpp
@@ -5,10 +5,11 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 
-// Standard C Libraries
-#include <stdbool.h>
+// Standard C/C++ Libraries
+#include <stdbool.h> // Required for : bool
 #include <cstdlib>  // Required for : getenv
-#include <chrono>
+#include <time.h> // Required for : nanosleep
+#include <chrono> // Required for : std::chrono
 using namespace std::chrono;
 
 // ServerLoop
@@ -17,6 +18,7 @@ static bool serverIsRunning = true;
 // TickRates
 static int tickRate = 60;
 static unsigned long tickDuration; // Duration of each tick in nanoseconds
+static double fixedDeltaTime = 0.0; // Delta fixe pour updates
 
 // Delta time
 static double rcnet_deltaTime = 0;
@@ -53,6 +55,17 @@ static bool rcnet_engine_initOpenssl(void)
     }
 }
 
+/**
+ * \brief Initialise le client JWT avec la clé publique et l'émetteur.
+ * 
+ * Cette fonction récupère la clé publique JWT depuis une variable d'environnement,
+ * la décode en PEM, puis initialise le client JWT. Elle loggue les erreurs et
+ * retourne false si l'initialisation échoue.
+ * 
+ * \return {bool} - true si l'initialisation réussit, false sinon
+ *
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 static bool rcnet_engine_initClientJWT(void)
 {
     const char* keyJWTBase64 = getenv("SEATYRANTS_PUBLIC_KEY_JWT_BASE64");
@@ -82,21 +95,30 @@ static bool rcnet_engine_initClientJWT(void)
 }
 
 /**
- * \brief Obtient le temps actuel en nanosecondes.
+ * \brief Obtient le temps actuel en nanosecondes depuis l'époque.
  * 
- * Cette fonction utilise `clock_gettime` pour obtenir le temps actuel
- * depuis l'horloge monotone, ce qui est utile pour les calculs de temps
- * dans les boucles de jeu.
+ * Cette fonction utilise chrono pour obtenir le temps actuel en nanosecondes.
  * 
- * \return {unsigned long} - Le temps actuel en nanosecondes.
+ * \return {uint64_t} - Temps actuel en nanosecondes.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
  */
-static unsigned long rcnet_engine_getCurrentTimeNs(void)
+static uint64_t rcnet_engine_getCurrentTimeNs(void)
 {
     auto now = steady_clock::now();
-    return duration_cast<nanoseconds>(now.time_since_epoch()).count();
+    return static_cast<uint64_t>(duration_cast<nanoseconds>(now.time_since_epoch()).count());
 }
 
-// Set Callbacks Engine by User
+
+/**
+ * \brief Définit les callbacks du moteur de jeu.
+ * 
+ * Cette fonction permet de définir les callbacks pour le moteur de jeu.
+ * 
+ * \param callbacksUser {RCNET_Callbacks*} - Pointeur vers la structure des callbacks à définir.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 static void rcnet_engine_setCallbacks(RCNET_Callbacks* callbacksUser) 
 {
     // Game loop callbacks
@@ -105,6 +127,15 @@ static void rcnet_engine_setCallbacks(RCNET_Callbacks* callbacksUser)
     if (callbacksUser->rcnet_update != NULL) callbacksServerEngine.rcnet_update = callbacksUser->rcnet_update;
 }
 
+/**
+ * \brief Initialise le moteur de jeu RCNET.
+ * 
+ * Cette fonction initialise OpenSSL, le client JWT et la durée des ticks.
+ * 
+ * \return {bool} - true si l'initialisation réussit, false sinon
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 static bool rcnet_engine(void)
 {
     // Initialize OpenSSL
@@ -119,46 +150,80 @@ static bool rcnet_engine(void)
         return false;
     }
 
-    // Initialize tickDuration
+    // Initialize tickDuration & fixedDeltaTime
     tickDuration = 1000000000 / tickRate;
+    fixedDeltaTime = 1.0 / static_cast<double>(tickRate);
 
 	return true;
 }
 
-static void rcnet_engine_serverloop(unsigned long* last_time) 
+/**
+ * \brief Boucle principale du serveur.
+ * 
+ * Cette fonction gère la boucle principale du serveur, en appelant les callbacks
+ * d'update à chaque tick et en gérant le timing pour respecter le tickRate.
+ * 
+ * \param next_tick_time {unsigned long*} - Pointeur vers le temps du prochain tick.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
+static void rcnet_engine_serverloop(unsigned long* next_tick_time)
 {
-    // Start Time ServerLoop
-    unsigned long start_time = rcnet_engine_getCurrentTimeNs();
-    
-    // Calculate deltaTime
-    rcnet_deltaTime = (start_time - *last_time) / 1000000000.0; // Convert ns to seconds
-    *last_time = start_time;
+    unsigned long now = rcnet_engine_getCurrentTimeNs();
 
-    // Run Callbacks Update
-    if (callbacksServerEngine.rcnet_update != NULL) 
+    // Si on est en avance, on dort jusqu'au tick prévu
+    if (now < *next_tick_time) 
     {
-        callbacksServerEngine.rcnet_update(rcnet_deltaTime);
-    }
-
-    // End Time ServerLoop
-    unsigned long end_time = rcnet_engine_getCurrentTimeNs();
-    unsigned long elapsed_time = end_time - start_time;
-    if (elapsed_time < tickDuration) 
-    {
-        unsigned long sleep_time = tickDuration - elapsed_time;
+        unsigned long sleep_time = *next_tick_time - now;
         struct timespec ts;
         ts.tv_sec = sleep_time / 1000000000;
         ts.tv_nsec = sleep_time % 1000000000;
         nanosleep(&ts, NULL);
+        now = rcnet_engine_getCurrentTimeNs(); // recheck après sleep
+    }
+
+    // deltaTime réel (en secondes)
+    rcnet_deltaTime = (now - (*next_tick_time - tickDuration)) / 1e9;
+
+    // Appeler la fonction d’update du serveur
+    if (callbacksServerEngine.rcnet_update != NULL)
+    {
+        callbacksServerEngine.rcnet_update(fixedDeltaTime);
+    }
+
+    // Planifier le prochain tick
+    *next_tick_time += tickDuration;
+
+    // Si on a pris du retard, corriger
+    if (*next_tick_time < rcnet_engine_getCurrentTimeNs())
+    {
+        rcnet_logger_log(RCNET_LOG_WARN, "Le serveur est en retard d’un tick !");
+        *next_tick_time = rcnet_engine_getCurrentTimeNs(); // reset sur maintenant
     }
 }
 
+/**
+ * \brief Initialise le moteur de jeu RCNET.
+ * 
+ * Cette fonction initialise le moteur de jeu RCNET.
+ * 
+ * \return {bool} - true si l'initialisation réussit, false sinon
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 static bool rcnet_engine_init(void)
 {
 	// Init ServeEngine house
 	return rcnet_engine();
 }
 
+/**
+ * \brief Quitte le moteur de jeu RCNET.
+ * 
+ * Cette fonction libère les ressources utilisées par le moteur de jeu RCNET.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 static void rcnet_engine_quit(void)
 {    
     // Lib OpenSSL Deinitialize
@@ -168,12 +233,32 @@ static void rcnet_engine_quit(void)
     SSL_COMP_free_compression_methods();
 }
 
+/**
+ * \brief Événement de sortie du moteur de jeu RCNET.
+ * 
+ * Cette fonction est appelée lorsque le moteur de jeu RCNET doit quitter.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
 void rcnet_engine_eventQuit(void)
 {
     serverIsRunning = false;
 }
 
-bool rcnet_engine_run(RCNET_Callbacks* callbacksUser, int tickRate)
+/**
+ * \brief Démarre le moteur de jeu RCNET avec les callbacks et le tick rate spécifiés.
+ * 
+ * Cette fonction configure les callbacks du moteur de jeu, initialise le moteur,
+ * et lance la boucle principale du serveur.
+ * 
+ * \param callbacksUser {RCNET_Callbacks*} - Pointeur vers les callbacks à utiliser.
+ * \param tickRate {int} - Taux de ticks par seconde (par défaut 60).
+ * 
+ * \return {bool} - true si le moteur démarre avec succès, false sinon.
+ * 
+ * \since Cette fonction est disponible depuis RCNET 1.0.0.
+ */
+bool rcnet_engine_run(RCNET_Callbacks* callbacksUser, int tickRateArg)
 {
     // Set Callbacks to Server Engine
     if (callbacksUser != NULL)
@@ -182,10 +267,11 @@ bool rcnet_engine_run(RCNET_Callbacks* callbacksUser, int tickRate)
     }
 
     // Set TickRate
-    if (tickRate > 0)
+    if (tickRateArg > 0)
     {
-        ::tickRate = tickRate;
-        tickDuration = 1000000000 / ::tickRate;
+        ::tickRate = tickRateArg;
+        tickDuration = 1000000000UL / ::tickRate;
+        fixedDeltaTime = 1.0 / ::tickRate;
     }
 
     // Init GameEngine RCNET
@@ -202,10 +288,10 @@ bool rcnet_engine_run(RCNET_Callbacks* callbacksUser, int tickRate)
     }
 
     // ServerLoop - Main thread
-    unsigned long last_time = rcnet_engine_getCurrentTimeNs();
+    unsigned long next_tick_time = rcnet_engine_getCurrentTimeNs() + tickDuration;
     while (serverIsRunning) 
     {
-        rcnet_engine_serverloop(&last_time);
+        rcnet_engine_serverloop(&next_tick_time);
     }
 
     // Last call the callback to unload the server loop (free memory, etc.)
